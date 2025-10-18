307ebf019df0c1708f11f0e01b64555a09fe39df
taylor <bryan.h.pro@gmail.com>
2025-10-18 16:37:55 +0200

    finalize maintenance page

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -1,23 +1,19 @@
-import Image from 'next/image'
+export const metadata = {
+    title: 'Maintenance',
+    description: 'Site en maintenance'
+}
+
+// Cette page n'utilisera pas le layout par défaut
+export const dynamic = 'force-static'
+
 export default function Maintenance() {
     return (
-        <html>
-            <body>
-                <main className="flex h-dvh flex-col justify-center items-center bg-[#f7f2ee]">
-                    {/* <Image
-                        src="/logoDpdBlack.png"
-                        alt="logo"
-                        width={500}
-                        height={300}
-                        className="w-full h-[400px]"
-                    /> */}
-                    <h1 className="text-[50px] font-bold">Maintenance en cours</h1>
-                    <p className="text-[30px]">
-                        Notre site est actuellement en maintenance. Revenez
-                        bientôt !
-                    </p>
-                </main>
-            </body>
-        </html>
+        <main className="flex h-dvh flex-col justify-center items-center bg-[#f7f2ee]">
+            <h1 className="text-[50px] font-bold">Maintenance en cours</h1>
+            <p className="text-[30px]">
+                Notre site est actuellement en maintenance. Revenez
+                bientôt !
+            </p>
+        </main>
     )
 }
